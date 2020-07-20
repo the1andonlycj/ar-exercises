6bda7791f360809b6be2c0e4e4f3fca0f5b26e42
Charlie Hayes <cjhayestutor@gmail.comgit config --global user.email cjhayestutor@gmail.com>
2020-07-20 23:02:28 +0000

    Setup the project and completed the first exercise.

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,12 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+# "stores" ("id" serial primary key, "name" character varying, "annual_revenue" integer, 
+# "mens_apparel" boolean, "womens_apparel" boolean, 
+# "created_at" timestamp NOT NULL, "updated_at" timestamp NOT NULL)
+
+Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+puts Store.count
